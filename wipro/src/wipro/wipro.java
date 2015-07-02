577d583553d006e852480a1f270dc9b6f07ad17d
1sand0s <1sand0sardpi@gmail.com>
2015-07-02 18:17:21 +0530

    JAVA_CODE_TO_OBTAIN_1_AND_0

---
+++ wipro/src/wipro/wipro.java
@@ -0,0 +1,41 @@
+package wipro;
+
+import java.awt.image.*;
+
+import javax.imageio.ImageIO;
+import java.io.*;
+public class wipro 
+{
+
+	public static void main(String[] args)throws IOException
+	{
+		new wipro();
+	}
+	wipro()throws IOException
+	{
+		int[] pix;
+		File f=new File("C:/Users/Computer/Desktop/test.jpg");
+		BufferedImage i=ImageIO.read(f);
+		WritableRaster r=i.getRaster();
+		pix=new int[r.getHeight()*r.getWidth()];
+		r.getPixels(0,0,r.getWidth(),r.getHeight(), pix);
+			
+		for(int y=0;y<r.getHeight();y++)
+		{
+			for(int x=0;x<r.getWidth();x++)
+			{
+				if(pix[x+y*r.getWidth()]>10)
+				{
+					pix[x+y*r.getWidth()]=1;
+				}
+				else
+				{
+					pix[x+y*r.getWidth()]=0;
+				}
+				System.out.print(pix[x+y*r.getWidth()]+" , ");
+			}
+			System.out.println("");
+		}
+	}
+
+}
